5f6408b3b3b8d854f9502fac17d30a27004b78f8
abrham17 <abrhamhabtom17@gmail.com>
2025-01-28 18:23:00 +0300

    worked on image translation

--- sign_translator/settings.py
+++ sign_translator/settings.py
@@ -4,8 +4,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#q8%@q-iz%a_2b@6tl$a6vzx0&zki_ufc#2m*nv$=-sbr#v6tw'
