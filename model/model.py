2efffe29fb343318e02064a352d1e6808019f18d
abrham17 <abrhamhabtom17@gmail.com>
2025-02-02 12:24:21 +0300

    finallized the encoder block

--- model/model.py
+++ model/model.py
@@ -96,16 +96,27 @@ class ResidualConnection(nn.Module):
     def forward(self, x):
         return x + self.dropout(self.layer(self.norm(x)))
 
-
 class EncoderBlock(nn.Module):
+
     def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForward, dropout: float) -> None:
         super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connection_1 =ResidualConnection(features, dropout, self_attention_block) 
-        self.residual_connection_2 = ResidualConnection(features, dropout, feed_forward_block)
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(2)])
 
-    def forward(self , x):
-        x = self.residual_connection_1(x, self.self_attention_block.attention_scores)
-        x = self.residual_connection_2(x ,self.feed_forward_block)
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
         return x
+    
+class Encoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
