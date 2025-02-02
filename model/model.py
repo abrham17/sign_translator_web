27c94d418f35ed9429698c1ce40cef79feec7633
abrham17 <abrhamhabtom17@gmail.com>
2025-02-02 12:19:45 +0300

    finallized the encoder block

--- model/model.py
+++ model/model.py
@@ -25,3 +25,87 @@ class PositionalEncodding(nn.Module):
         self.register_buffer('positional_encoding', self.positional_encoding)
     def forward(self, x):
         return self.dropout(x + (self.positional_encoding[:, :x.size(1), :]).requires_grad_(False))
+
+
+class LayerNormalization(nn.Module):
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) 
+        self.bias = nn.Parameter(torch.zeros(features))
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForward(nn.Module):
+    def __init__(self , d_model=512 , d_ff = 2048):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout()
+        self.linear2 = nn.Linear(d_ff, d_model)
+    def forward(self, x):
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model # Embedding vector size
+        self.h = h # Number of heads
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model, bias=False) # Wq
+        self.w_k = nn.Linear(d_model, d_model, bias=False) # Wk
+        self.w_v = nn.Linear(d_model, d_model, bias=False) # Wv
+        self.w_o = nn.Linear(d_model, d_model, bias=False) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Write a very low value (indicating -inf) to the positions where mask == 0
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        # Calculate attention
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+    def __init__(self, d_model: int, dropout: float, layer: nn.Module) -> None:
+        super().__init__()
+        self.layer = layer
+        self.norm = LayerNormalization(d_model)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        return x + self.dropout(self.layer(self.norm(x)))
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForward, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection_1 =ResidualConnection(features, dropout, self_attention_block) 
+        self.residual_connection_2 = ResidualConnection(features, dropout, feed_forward_block)
+
+    def forward(self , x):
+        x = self.residual_connection_1(x, self.self_attention_block.attention_scores)
+        x = self.residual_connection_2(x ,self.feed_forward_block)
+        return x
